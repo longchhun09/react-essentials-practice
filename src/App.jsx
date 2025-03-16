53d8e05173f015a225927a41c99f72394c072dd3
mark chhunlong <mark.chhunlong@gmail.com>
2025-03-16 09:49:15 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Result";
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 100000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((preUserInput) => {
+      return {
+        ...preUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+  return (
+    <>
+      <Header />;
+      <UserInput onChange={handleChange} userInput={userInput} />;
+      <Results />
+    </>
+  );
+}
+
+export default App;
