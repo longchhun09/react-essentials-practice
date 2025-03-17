16dbf762d37c88e9c795b2742aafa50bce8fa8cc
mark chhunlong <mark.chhunlong@gmail.com>
2025-03-17 02:49:17 +0000

    task: validate duration input

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     expectedReturn: 6,
     duration: 10,
   });
+  const inputIsValid = userInput.duration >= 1;
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((preUserInput) => {
@@ -24,7 +25,8 @@ function App() {
     <>
       <Header />;
       <UserInput onChange={handleChange} userInput={userInput} />;
-      <Results input={userInput} />
+      {!inputIsValid && <p>Please input valid data</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
